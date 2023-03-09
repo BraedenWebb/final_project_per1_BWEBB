a719fc4f43b2593aeb9c6c3cc4e28e50bcfc039c
BraedenWebb <126900985+BraedenWebb@users.noreply.github.com>
2023-03-09 12:01:51 -0800

    Add files via upload

---
+++ my_game/settings.py
@@ -0,0 +1,17 @@
+# Filed created by Braeden Webb
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# Game settings
+FPS = 60
+RUNNING = True
+
+# player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.12
+
+# colors
+BLACK = (0,0,0)
+BLUE = (25,25,255)
