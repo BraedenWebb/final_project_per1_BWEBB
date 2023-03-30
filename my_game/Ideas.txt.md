2d84e098b69273a1eff8635b3ab69223edf90f22
BraedenWebb <braedenwebb23@gmail.com>
2023-03-30 14:26:14 -0700

    extra file

---
+++ my_game/Ideas.txt.md
@@ -0,0 +1,15 @@
+Goals, Rules, Feedback, Freedom
+
+## Goals ##
+- Make top down shooter game
+- Have particles fire to mouse
+- Have sprites
+
+## Rules ##
+- Player sti
+
+## Feedback ##
+- 
+
+## Freedom ##
+- 
--- my_game/settings.py
+++ my_game/settings.py
@@ -1,26 +1,29 @@
 # Filed created by Braeden Webb
 
-# screen dimensions
 WIDTH = 800
 HEIGHT = 600
 
-# Game settings
+PLAYER_ACC = .5
+PLAYER_FRICTION = -0.5
+PLAYER_JUMP = 20
+PLAYER_GRAV = 0
+
+MOB_ACC = 2
+MOB_FRICTION = -0.3
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+RED = (255,0,50)
+
 FPS = 60
 RUNNING = True
+SCORE = 0
+PAUSED = False
 
-# player attributes
-PLAYER_ACC  = .5
-PLAYER_FRICTION = -.05
-PLAYER_JUMP = 15
-PLAYER_GRAV = 0.98
-
-# mob attributes
-MOB_ACC = .025
-MOB_FRICTION = .012
-
-# colors
-BLACK = (0,0,0)
-BLUE = (0,0,255)
-RED = (255,0,0)
-WHITE = (255,255,255)
-GREEN = (0,255,0)
+# Starting platforms
+# PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
+#                  (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (100,255,100), "bouncey"),
+#                  (125, HEIGHT - 350, 100, 5, (200,100,50), "disappearing"),
+#                  (350, 200, 100, 20, (200,200,200), "normal"),
+#                  (175, 100, 50, 20, (200,200,200), "normal")]
